3bb5e868f5f528e4d120ca77a037d2f5759785c8
adnanGrebovic <adnan.grebovic.ag@gmail.com>
2024-01-10 01:09:38 +0100

    first commit

---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,16 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class StoreContext:DbContext
+    {
+        // internal ActionResult<Product> saveProducts;
+
+        public StoreContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+    }
+}
